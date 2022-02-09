a8bf532c49459d42952da39372ead5ac7f74442a
TR <89931570+TaniaRevenok@users.noreply.github.com>
2022-02-09 19:13:38 +0200

    Create sql4.1.sql

---
+++ sql4.1.sql
@@ -0,0 +1,8 @@
+UPDATE projects SET cost = 200000 WHERE (projects.id = 1);
+UPDATE projects SET cost = 250000 WHERE (projects.id = 2);
+UPDATE projects SET cost = 180000 WHERE (projects.id = 3);
+UPDATE projects SET cost = 300000 WHERE (projects.id = 4);
+UPDATE projects SET cost = 230000 WHERE (projects.id = 5);
+UPDATE projects SET cost = 100000 WHERE (projects.id = 6);
+UPDATE projects SET cost = 330000 WHERE (projects.id = 7);
+UPDATE projects SET cost = 210000 WHERE (projects.id = 8);
